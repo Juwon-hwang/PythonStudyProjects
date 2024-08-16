c7ab56b6f1c259a61dbf9e98e31901e12fec86b7
Juwon-hwang <57760393+Juwon-hwang@users.noreply.github.com>
2024-08-16 17:25:23 +0900

    Add files via upload

---
+++ PythonProject/ping-pong_game/main.py
@@ -0,0 +1,56 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Ping-Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.wall_bounce()
+
+    #Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.paddle_bounce()
+    
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect L paddle misses        
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+    
+    #Game over  
+    if scoreboard.l_score == 1:
+        scoreboard.game_over(winner="Left")
+        game_is_on = False
+    if scoreboard.r_score == 1:
+        scoreboard.game_over(winner="Right")
+        game_is_on = False
+        
+screen.exitonclick()
